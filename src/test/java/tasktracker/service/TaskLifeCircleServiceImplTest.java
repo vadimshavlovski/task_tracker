a1ae18ee8f8ee9219ddd29db6aacb9273028a669
Vadim Shavlovski <vadimshavlovski@mail.ru>
2018-05-10 20:29:35 +0300

    refactor Test for LifeCircleService

--- src/test/java/tasktracker/service/TaskLifeCircleServiceImplTest.java
+++ src/test/java/tasktracker/service/TaskLifeCircleServiceImplTest.java
@@ -14,22 +14,27 @@ public class TaskLifeCircleServiceImplTest {
 
     @Test
     public void setToInProgressTaskTest() throws Exception {
+
         task.setStatus(TaskStatus.NEW);
         taskLifeCircleService.setToInProgress(task);
+
         assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
     }
 
     @Test
     public void setToInProgressImplementedTaskTest() throws Exception {
+
         task.setStatus(TaskStatus.IMPLEMENTED);
         taskLifeCircleService.setToInProgress(task);
+
         assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
     }
 
     @Test(expected = InvalidTaskStateException.class)
     public void getInvalidTaskStateTest() throws InvalidTaskStateException{
-        task.setStatus(TaskStatus.CLOSED);
-        taskLifeCircleService.implement(task);
 
+        task.setStatus(TaskStatus.CLOSED);
+
+        taskLifeCircleService.implement(task);
     }
 }
