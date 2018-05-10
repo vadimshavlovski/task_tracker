d905bffe156d3926fa82ac875d527678b5d1c0ea
vadimshavlovski <33630129+vadimshavlovski@users.noreply.github.com>
2018-05-10 20:25:46 +0300

    added Test for LifeCircleService

---
+++ src/test/java/tasktracker/service/TaskLifeCircleServiceImplTest.java
@@ -0,0 +1,35 @@
+package tasktracker.service;
+
+import org.junit.Test;
+import tasktracker.domain.Task;
+import tasktracker.domain.TaskStatus;
+import tasktracker.exception.InvalidTaskStateException;
+
+import static org.junit.Assert.*;
+
+public class TaskLifeCircleServiceImplTest {
+
+    private static TaskLifeCircleServiceImpl taskLifeCircleService = new TaskLifeCircleServiceImpl();
+    private static Task task = new Task();
+
+    @Test
+    public void setToInProgressTaskTest() throws Exception {
+        task.setStatus(TaskStatus.NEW);
+        taskLifeCircleService.setToInProgress(task);
+        assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
+    }
+
+    @Test
+    public void setToInProgressImplementedTaskTest() throws Exception {
+        task.setStatus(TaskStatus.IMPLEMENTED);
+        taskLifeCircleService.setToInProgress(task);
+        assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
+    }
+
+    @Test(expected = InvalidTaskStateException.class)
+    public void getInvalidTaskStateTest() throws InvalidTaskStateException{
+        task.setStatus(TaskStatus.CLOSED);
+        taskLifeCircleService.implement(task);
+
+    }
+}
